aaf2313aacd8f387efab614c3814f8fa06a87238
Matt Shirley <mdshw5@gmail.com>
2018-05-01 14:32:49 -0400

    Generalize example script

--- example/run_FreePSI.sh
+++ example/run_FreePSI.sh
@@ -2,23 +2,6 @@
 
 set -e
 
-# Provide the directory containing Jellyfish (usually named as 'bin')  
-Jellyfish=
-# Provide the directory containing FreePSI
-# E.g. 
-#FreePSI=../bin
-FreePSI=
-
-if [ -z ${Jellyfish} ]; then
-    echo "Error: Please modify me to provide the directory containing Jellyfish"
-    exit;
-fi
-
-if [ -z ${FreePSI} ]; then
-    echo "Error: Please modify me to provide the directory containing FreePSI"
-    exit;
-fi
-
 GENOME_DIR=./genome
 BND_FILE=./annotation/hg38_refGene_exonBoundary_chr21.bed
 READS=RNA-seq
@@ -27,14 +10,14 @@ THREAD=4
 
 set -x
 # Count k-mers in RNA-seq reads using jellyfish
-${Jellyfish}/jellyfish count -m ${K} -s 100M -t ${THREAD} -Q 5 ${READS}/reads_final.1.fastq -o ${READS}/reads.1.jf
-${Jellyfish}/jellyfish dump ${READS}/reads.1.jf -o ${READS}/reads.1.fa
-${Jellyfish}/jellyfish count -m ${K} -s 100M -t ${THREAD} -Q 5 ${READS}/reads_final.2.fastq -o ${READS}/reads.2.jf
-${Jellyfish}/jellyfish dump ${READS}/reads.2.jf -o ${READS}/reads.2.fa
+jellyfish count -m ${K} -s 100M -t ${THREAD} -Q 5 ${READS}/reads_final.1.fastq -o ${READS}/reads.1.jf
+jellyfish dump ${READS}/reads.1.jf -o ${READS}/reads.1.fa
+jellyfish count -m ${K} -s 100M -t ${THREAD} -Q 5 ${READS}/reads_final.2.fastq -o ${READS}/reads.2.jf
+jellyfish dump ${READS}/reads.2.jf -o ${READS}/reads.2.fa
 
 # Produce raw estimates of PSI values using FreePSI
 #Build
-${FreePSI}/freePSI build\
+freePSI build\
     -k $K -p ${THREAD} \
     -g ${GENOME_DIR} \
     -a ${BND_FILE} \
@@ -43,18 +26,18 @@ ${FreePSI}/freePSI build\
     -o ./hashtable.json
 
 #Quant
-${FreePSI}/freePSI quant\
+freePSI quant\
     -k $K -p ${THREAD} \
     -i ./hashtable.json \
     -o .
 
 # Post-process the raw estimates of PSI values
-python3 ${FreePSI}/postProc.py \
+python3 freePSI-postProc.py \
     ./psi_freePSI_raw.json \
     ./psi_freePSI.json
 
 # Summarize the PSI values into a readable file
-python3 ${FreePSI}/summary.py \
+python3 freePSI-summary.py \
     ./annotation/hg38_refGene_exonBoundary_chr21.bed \
     ./psi_freePSI.json \
-    ./psi_freePSI.summary 
+    ./psi_freePSI.summary
--- src-refined/freePSI-postProc.py
+++ src-refined/freePSI-postProc.py
@@ -1,3 +1,5 @@
+#! /bin/env python3
+
 import json
 import math
 import numpy as np
